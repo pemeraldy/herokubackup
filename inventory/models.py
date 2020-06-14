c65a6ff901f79ca81bc4bc8dad0f216003b1b8fc
unknown <pemeraldy@gmail.com>
2020-06-15 00:10:13 +0100

    added new models

--- inventory/models.py
+++ inventory/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
 class Product(models.Model):
-    PRODUCT_TYPE = (
-        ('Phone', 'Phone'),
-        ('Laptop','Laptop'),
-        ('Chargers', 'Chargers')
-    )
+    # PRODUCT_TYPE = (
+    #     ('Phone', 'Phone'),
+    #     ('Laptop','Laptop'),
+    #     ('Chargers', 'Chargers')
+    # )
     CATEGORY = (
         ('Incoming','Incoming'),
         ('Outgoing', 'Outgoing'),
         ('Available','Available')
     )
     name = models.CharField(max_length=200, null=True)
-    product_type = models.CharField(max_length=200, null=True, choices=PRODUCT_TYPE)
+    product_tag = models.ManyToManyField(Tag)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     quantity = models.IntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
@@ -21,5 +26,21 @@ class Product(models.Model):
     reoder_level = models.IntegerField(null=True)
     def __str__(self):
         return self.name
+
+class Sales(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Delivered', 'Delivered')
+    )
+    product = models.ForeignKey(Product,null=True, on_delete=models.SET_NULL)
+    customer_name = models.CharField(max_length=200, null=True)
+    customer_email = models.CharField(max_length=200, null=True)
+    quantity = quantity = models.IntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    amount = models.FloatField(null=True)
+
+    def __str__ (self):
+        return self.customer_name + self.product
 
 
